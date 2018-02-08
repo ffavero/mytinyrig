7643fcdc44003a9903f6bed73445b271fdcd8130
Francesco Favero <francesco.favero@bric.ku.dk>
2018-02-08 15:27:34 +0100

    add a possible processing function

--- mytinyrig/processing.py
+++ mytinyrig/processing.py
@@ -1,1 +1,22 @@
+import multiprocessing
 import subprocess
+import shlex
+
+
+class process:
+
+    def __init__(self, command):
+        command = shlex.split(command)
+        self.proc = subprocess.Popen(command, stdout=subprocess.PIPE,
+                                     bufsize=4096)
+
+    def __iter__(self):
+        for line in self.proc.stdout:
+            yield line.decode('utf-8')
+
+    def pid(self):
+        return self.proc.pid
+
+    def close(self):
+        kill_subproc(self.proc)
+        self.proc.stdout.close()
