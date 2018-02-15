b729f284089242e87eff75ce33bad36efd507070
Francesco Favero <francesco.favero@bric.ku.dk>
2018-02-15 22:40:56 +0100

    fix log path

--- mytinyrig/logs.py
+++ mytinyrig/logs.py
@@ -6,11 +6,11 @@ class log:
 
     def __init__(self, name, path, level=logging.INFO):
         self.__name__ = name
-        self.__path__ = os.path.join(path, self.__name__)
+        self.__path__ = os.path.abspath(path)
         if not os.path.exists(self.__path__):
             os.makedirs(self.__path__)
         self.log = logging.getLogger(name)
-        self.log_file = '%s.log' % self.__path__
+        self.log_file = '%s.log' % os.path.join(self.__path__, self.__name__)
         formatter = logging.Formatter(
             self.__name__ + ': %(levelname)s : %(asctime)s : %(message)s')
         fileHandler = RotatingFileHandler(
