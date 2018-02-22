5d11bf0d1e0a63e0c3daf75c02faf620122946e3
Francesco Favero <francesco.favero@bric.ku.dk>
2018-02-22 14:53:05 +0100

    Change configuration loading: allow to set a specified folder from args

--- mytinyrig/__config__.py
+++ mytinyrig/__config__.py
@@ -1,13 +1,16 @@
 import os
 from mytinyrig.misc import try_import
 
-MY_WORKERS = os.environ.get('MY_WORKERS')
 
-if MY_WORKERS:
+def get_config(configdir):
+
+    if configdir is not None:
+        MY_WORKERS = configdir
+    elif os.environ.get('MY_WORKERS'):
+        MY_WORKERS = os.environ.get('MY_WORKERS')
+    else:
+        base, name = os.path.split(os.path.abspath(__file__))
+        MY_WORKERS = os.path.join(base, 'workers')
     base, name = os.path.split(os.path.abspath(MY_WORKERS))
     my_workers = try_import(base, name)
-    MY_WORKERS = my_workers
-else:
-    base, name = os.path.split(os.path.abspath(__file__))
-    my_workers = try_import(base, 'workers')
-    MY_WORKERS = my_workers
+    return my_workers
